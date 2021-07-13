7129c5202119d4586f7a9288cc8c957d620ce7a7
rronkurtishi <rron.kurtishi@gmail.com>
2021-07-13 17:47:48 +0200

    Generated main components and folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CostsPageComponent } from './costs-page/costs-page.component';
+import { ListComponent } from './costs-page/list/list.component';
+import { ListItemComponent } from './costs-page/list/list-item/list-item.component';
+import { CommentComponent } from './costs-page/list/list-item/comment/comment.component';
+import { NewCommentComponent } from './costs-page/list/list-item/new-comment/new-comment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CostsPageComponent,
+    ListComponent,
+    ListItemComponent,
+    CommentComponent,
+    NewCommentComponent
   ],
   imports: [
     BrowserModule,
