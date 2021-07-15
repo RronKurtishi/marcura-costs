65ae2e15dbaa51480294959a37f34fd539afe20c
rronkurtishi <rron.kurtishi@gmail.com>
2021-07-15 21:36:55 +0200

    Implemented initial list UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CostsPageComponent } from './costs-page/costs-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: CostsPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
