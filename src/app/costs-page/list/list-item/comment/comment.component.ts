381f8bb0005ebad2ac9e836ad7b255bcd781b443
rronkurtishi <rron.kurtishi@gmail.com>
2021-07-19 13:06:27 +0200

    Added data to comment component ui

--- src/app/costs-page/list/list-item/comment/comment.component.ts
+++ src/app/costs-page/list/list-item/comment/comment.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CostItemComment } from 'src/app/core/interfaces/cost-item-comment';
 
 @Component({
   selector: 'app-comment',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comment.component.scss']
 })
 export class CommentComponent implements OnInit {
+  @Input() comment: CostItemComment;
 
   constructor() { }
 
