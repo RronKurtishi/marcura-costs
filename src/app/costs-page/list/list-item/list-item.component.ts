7a736c596f983b964e8078ffb66c870849c291b1
rronkurtishi <rron.kurtishi@gmail.com>
2021-07-19 13:04:33 +0200

    Added data to list and list item components and displayed them in the ui

--- src/app/costs-page/list/list-item/list-item.component.ts
+++ src/app/costs-page/list/list-item/list-item.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CostItem } from 'src/app/core/interfaces/cost-item';
+import { CostItemCost } from 'src/app/core/interfaces/cost-item-cost';
 
 @Component({
   selector: 'app-list-item',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-item.component.scss']
 })
 export class ListItemComponent implements OnInit {
+  @Input() costItem: CostItem;
+  @Input() currentCurrency: any;
+  @Input() daCurrency: any;
 
+  public screened: CostItemCost;
+  public quoted: CostItemCost;
+  public showComments: boolean;
+  
   constructor() { }
 
   ngOnInit(): void {
+    this.quoted = this.costItem.costs.find(exp => exp.type === 'Quoted')!;
+    this.screened = this.costItem.costs.find(exp => exp.type === 'Screened')!;
   }
 
 }
--- src/app/costs-page/list/list.component.ts
+++ src/app/costs-page/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-list',
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
+  @Input() cost: any;
+  @Input() currentCurrency: any;
+  @Input() daCurrency: any;
 
-  constructor() { }
+  public totalQuoted: number;
+  public totalScreened: number;
+
+  constructor() {
+    this.totalQuoted = 0;
+    this.totalScreened = 0
+   }
 
   ngOnInit(): void {
+    this.cost.costItems.forEach((costItem: any) => {
+      const quoted = costItem.costs.find((exp: any) => exp.type === 'Quoted').amount;
+      this.totalQuoted += quoted;
+      const screened = costItem.costs.find((exp: any) => exp.type === 'Screened').amount;
+      this.totalScreened += screened;
+    })
   }
-
 }
